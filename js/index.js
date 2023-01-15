b2b76edec4bf433f802082259ac6faa73233daae
AR Nadal Verma <arnadal2010@gmail.com>
2023-01-15 19:53:08 +0530

    Major and Final Update!
    A lot of crucial updates, making us all ready!

--- js/index.js
+++ js/index.js
@@ -7,7 +7,51 @@ function showCopyright() {
   <ul>
     <li>Sittipat Tojarean</li>
     <li>Graphiqa</li>
-  </ul>`;
+    <li>Paul J.</li>
+    <li>Triangle Squad</li>
+    <li>Good Ware</li>
+    <li>max.icons</li>
+    <li>GOWI</li>
+    <li>Rashad</li>
+    <li>Freepik</li>
+  </ul>
+  Thanks to them!`;
+  NewPopup.setAttribute("style", `
+  position: fixed;
+  width: calc(100% / 3);
+  height: calc(100% / 2);
+  top: calc(100% / 3);
+  left: calc(100% / 3);
+  z-index: 65538;
+  background: var(--colorScheme3);
+  `);
+
+  NewWrapper.setAttribute("style", `
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    background: #000000D0;
+    z-index: 65537;
+    border-radius: 0;
+  `);
+  NewWrapper.onclick = function(event) {
+    NewPopup.remove();
+    NewWrapper.remove();
+  };
+
+  document.body.appendChild(NewWrapper)
+  document.body.appendChild(NewPopup);
+}
+
+function showThemeSwitcher() {
+  var NewPopup = document.createElement("div");
+  var NewWrapper = document.createElement("div");
+  NewPopup.innerHTML = `<h3>Themes</h3>
+  It shall suit your style.<br>
+  Currently, the theme is <b id="currentThemeName">Glacier</b>!<br>
+  <button onclick="toggleTheme()">Switch Theme</button>`;
   NewPopup.setAttribute("style", `
   position: fixed;
   width: calc(100% / 3);
--- js/themes.js
+++ js/themes.js
@@ -1,18 +1,17 @@
 var themes = [
   {
-    name: `Glacier (Default)`,
+    name: `Glacier`,
     colorScheme1: `rgb(16, 16, 16)`,
     colorScheme2: `rgb(177, 214, 255)`,
     colorScheme3: `rgb(118, 182, 255)`, colorScheme3_translucent: `rgba(118, 182, 255, 0.5)`,
     colorScheme4: `rgb(50, 135, 231)`, colorScheme4_translucent: `rgba(50, 135, 231, 0.5)`,
-    colorScheme5: `rgb(12, 86, 170)`,
-    colorScheme6: `rgb(19, 108, 209)`,
-    colorScheme7: `rgb(9, 93, 190)`,
-    colorScheme8: `rgb(8, 85, 173)`, colorScheme8_translucent: `rgba(8, 85, 173, 0.5)`,
-    colorScheme10: `rgb(202, 255, 103)`
+    colorScheme5: `rgb(13, 100, 199)`,
+    colorScheme6: `rgb(6, 85, 175)`,
+    colorScheme7: `rgb(5, 57, 116)`,
+    colorScheme8: `rgb(3, 44, 90)`, colorScheme8_translucent: `rgba(3, 44, 90, 0.5)`
   },
   {
-    name: `Tree Can't Flee`,
+    name: `Trees Can't Flee`,
     colorScheme1: `rgb(16, 16, 16)`,
     colorScheme2: `rgb(224, 248, 116)`,
     colorScheme3: `rgb(144, 240, 66)`, colorScheme3_translucent: `rgba(144, 240, 66, 0.5)`,
@@ -20,26 +19,24 @@ var themes = [
     colorScheme5: `rgb(108, 192, 39)`,
     colorScheme6: `rgb(85, 158, 26)`,
     colorScheme7: `rgb(60, 114, 15)`,
-    colorScheme8: `rgb(37, 70, 10)`, colorScheme8_translucent: `rgba(37, 70, 10, 0.5)`,
-    colorScheme10: `rgb(108, 128, 20)`
+    colorScheme8: `rgb(37, 70, 10)`, colorScheme8_translucent: `rgba(37, 70, 10, 0.5)`
   },
   {
-    name: `Tsunami`,
+    name: `Carbon Dioxide`,
     colorScheme1: `rgb(16, 16, 16)`,
-    colorScheme2: `rgb(177, 214, 255)`,
-    colorScheme3: `rgb(118, 182, 255`, colorScheme3_translucent: `rgba(118, 182, 255, 0.5)`,
-    colorScheme4: `rgb(50, 135, 231)`, colorScheme4_translucent: `rgba(50, 135, 231, 0.5)`,
-    colorScheme5: `rgb(12, 86, 170)`,
-    colorScheme6: `rgb(19, 108, 209)`,
-    colorScheme7: `rgb(9, 93, 190)`,
-    colorScheme8: `rgb(8, 85, 173)`, colorScheme8_translucent: `rgba(8, 85, 173, 0.5)`,
-    colorScheme10: `rgb(202, 255, 103)`
+    colorScheme2: `rgb(92, 92, 92)`,
+    colorScheme3: `rgb(145, 145, 145)`, colorScheme3_translucent: `rgba(118, 182, 255, 0.5)`,
+    colorScheme4: `rgb(130, 130, 130)`, colorScheme4_translucent: `rgba(130, 130, 130, 0.5)`,
+    colorScheme5: `rgb(92, 92, 92)`,
+    colorScheme6: `rgb(69, 69, 69)`,
+    colorScheme7: `rgb(56, 56, 56)`,
+    colorScheme8: `rgb(38, 38, 38)`, colorScheme8_translucent: `rgba(38, 38, 38, 0.5)`
   }
 ];
 var theme = 0;
 
-function setTheme(themeNum) {
-  console.log(`Switched to theme: ${themes[themeNum].name}`);
+function switchTheme(themeNum) {
+  document.querySelector("#currentThemeName").innerHTML = themes[themeNum].name;
   var r = document.querySelector(`:root`);
   r.style.setProperty(`--colorScheme1`, themes[themeNum].colorScheme1);
   r.style.setProperty(`--colorScheme2`, themes[themeNum].colorScheme2);
@@ -51,21 +48,21 @@ function setTheme(themeNum) {
   r.style.setProperty(`--colorScheme6`, themes[themeNum].colorScheme6);
   r.style.setProperty(`--colorScheme7`, themes[themeNum].colorScheme7);
   r.style.setProperty(`--colorScheme8`, themes[themeNum].colorScheme8);
-  r.style.setProperty(`--colorScheme8_translucent`, themes[themeNum].colorScheme8_traslucent);
-  r.style.setProperty(`--colorScheme10`, themes[themeNum].colorScheme10);
-  if (theme == themes.length - 1) {
+  r.style.setProperty(`--colorScheme8_translucent`, themes[themeNum].colorScheme8_translucent);
+  if (theme >= themes.length - 1) {
     theme = 0;
   } else {
     theme = theme + 1;
   }
+  
 }
 
 function toggleTheme() {
   if (theme == 0) {
-    setTheme(1);
+    switchTheme(1);
   } else if (theme == 1) {
-    setTheme(2);
+    switchTheme(2);
   } else if (theme == 2) {
-    setTheme(0);
+    switchTheme(0);
   }
 }
